00646e86a82d3ceadc80a9a7b6f4bd2cb2281d4d
Maksat <mtoktalievmegacom@gmail.com>
2022-10-07 11:39:52 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let answers = [];
+
+answers[0] = prompt('Your name', '');
+answers[1] = prompt('Last name', '');
+answers[2] = prompt('age', '');
+answers[3] = prompt('Adress', '');
+
+console.log(answers);
